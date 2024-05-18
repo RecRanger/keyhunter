6bf8e4f460291e888f6c8e456352b60ab569140f
RecRanger <168371178+RecRanger@users.noreply.github.com>
2024-05-17 20:55:27 -0600

    Apply repo setup and autoformat

--- keyhunter.py
+++ keyhunter.py
@@ -4,27 +4,26 @@
 import hashlib
 import sys
 
-# bytes to read at a time from file (10meg)
-readlength=10*1024*1024
+# bytes to read at a time from file (10 MiB)
+readlength = 10 * 1024 * 1024
 
-magic = '\x01\x30\x82\x01\x13\x02\x01\x01\x04\x20'
+magic = "\x01\x30\x82\x01\x13\x02\x01\x01\x04\x20"
 magiclen = len(magic)
 
 
-##### start code from pywallet.py #############
-
-__b58chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+__b58chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 __b58base = len(__b58chars)
 
-def b58encode(v):
-    """ encode v, which is a string of bytes, to base58.
-    """
 
-    long_value = 0L
-    for (i, c) in enumerate(v[::-1]):
+def b58encode(v):
+    """encode v, which is a string of bytes, to base58."""
+
+    # long_value = 0L
+    long_value = 0
+    for i, c in enumerate(v[::-1]):
         long_value += (256**i) * ord(c)
 
-    result = ''
+    result = ""
     while long_value >= __b58base:
         div, mod = divmod(long_value, __b58base)
         result = __b58chars[mod] + result
@@ -35,20 +34,21 @@ def b58encode(v):
     # leading 0-bytes in the input become leading-1s
     nPad = 0
     for c in v:
-        if c != '\0': 
+        if c != "\0":
             break
         nPad += 1
 
-    return (__b58chars[0]*nPad) + result
+    return (__b58chars[0] * nPad) + result
+
 
 def Hash(data):
     return hashlib.sha256(hashlib.sha256(data).digest()).digest()
+
 
 def EncodeBase58Check(secret):
     hash = Hash(secret)
     return b58encode(secret + hash[0:4])
 
-########## end code from pywallet.py ############
 
 def find_keys(filename):
     keys = set()
@@ -67,7 +67,7 @@ def find_keys(filename):
                 if pos == -1:
                     break
                 key_offset = pos + magiclen
-                key_data = "\x80" + data[key_offset:key_offset + 32]
+                key_data = "\x80" + data[key_offset : key_offset + 32]
                 keys.add(EncodeBase58Check(key_data))
                 pos += 1
 
@@ -77,14 +77,16 @@ def find_keys(filename):
                 f.seek(f.tell() - (32 + magiclen))
     return keys
 
+
 def main():
     if len(sys.argv) != 2:
-        print "./{0} <filename>".format(sys.argv[0])
+        print("./{0} <filename>".format(sys.argv[0]))
         exit()
 
     keys = find_keys(sys.argv[1])
     for key in keys:
-        print key
+        print(key)
+
 
 if __name__ == "__main__":
     main()
